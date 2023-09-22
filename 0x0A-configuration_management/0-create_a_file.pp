f72a3364dded9658f817a63fe8e9f45eccaca89e
Dennis Kimani <dxaldovah@gmail.com>
2023-09-22 04:08:19 -0700

    Using Puppet, create a file in /tmp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Ensure the creation of the /tmp/school file with specific attributes
+
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+This repo contains Configuration management projct tasks
